a041991aee5a7bd09b5cebb9c7eabc8dd648e693
Ateka Sultana <atekaoishi78@gmail.com>
2023-10-18 02:39:46 +0600

    client set up done

---
+++ src/Routes/Route.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout/MainLayout";
+import ErrorPage from "../Pages/ErrorPage";
+
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout></MainLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                
+            }
+        ]
+    }
+])
+
+export default router;
---
+++ src/layout/MainLayout/MainLayout.jsx
@@ -0,0 +1,10 @@
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h2>Main</h2>
+        </div>
+    );
+};
+
+export default MainLayout;
